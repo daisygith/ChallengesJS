867fb277ce5e9532eab908a31378d5ae48cd16a8
daisygith <daisygith@users.noreply.github.com>
2024-02-21 09:23:31 +0100

    first commit to first exercise

---
+++ ex1/animals.js
@@ -0,0 +1,10 @@
+function animals(chicken, cows, pigs){
+    let sum = (chicken *2) + (cows*4) + (pigs *4);
+    console.log(sum);
+    return sum;
+}
+animals(5,2,8);
+// console.log(animals(5,2,8));
+
+// (animals(5, 2, 8), 50)
+// (animals(1, 2, 3), 22)
