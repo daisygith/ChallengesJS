5047c645a4918ee3fd9a8782ea5acfe0966c44c0
daisygith <daisygith@users.noreply.github.com>
2024-02-28 19:53:28 +0100

    add solution to ex6

---
+++ JS/ex6/FixExpression.js
@@ -0,0 +1,12 @@
+let result;
+function isSeven(x) {
+    // return x="7"?false:true:false;
+    result = x===7?true:false;
+    console.log(result);
+    return result;
+}
+
+// isSeven(9); //false
+isSeven(7); //true
+// isSeven(4); //false
+
