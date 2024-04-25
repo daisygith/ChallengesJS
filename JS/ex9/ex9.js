0d1ea945f840989f08f1dadf90f330b7b118d58e
daisygith <daisygith@users.noreply.github.com>
2024-04-25 12:50:21 +0200

    added ex9

---
+++ JS/ex9/ex9.js
@@ -0,0 +1,7 @@
+let text = document.getElementById('text');
+
+function js_style() {
+    text.style.fontSize = '14px';
+    text.style.fontFamily = 'Comic Sans MS';
+    text.style.color = 'green';
+}
